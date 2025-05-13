329a491376405d2be3c5f9265f92e053ea46d668
Goutam <goutam.kathuria@gmail.com>
2025-05-13 17:24:39 +0530

    new commit

--- routes/route.js
+++ routes/route.js
@@ -2,9 +2,11 @@ const express = require('express');
 const upload = require('../midllerware/multer');
 const router = express.Router()
 
-const {addSub,intro,getIntro}=require('../controlers/controlers')
+const {addSub,intro,banner,getIntro,getBanner}=require('../controlers/controlers')
 
 router.post('/Product',upload.single('image'),addSub)
 router.post('/intro',upload.single('image'),intro)
+router.post('/banner',upload.single('image'),banner)
 router.get('/getIntro',getIntro)
+router.get('/getBanner',getBanner)
 module.exports=router;

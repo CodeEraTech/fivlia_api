75ed7db9d724e7f8a3f5c19647eeafb86339e40d
Goutam <goutam.kathuria@gmail.com>
2025-06-12 10:53:53 +0530

    done

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,7 +1,7 @@
 // firebaseConfig.js
 const admin = require('firebase-admin');
-const serviceAccount =  process.env.NODE_ENV === 'production'  ? require('/etc/secrets/fivlia-firebase-adminsdk-fbsvc-983c02ca3c.json')
-  : require('./fivlia-firebase-adminsdk-fbsvc-983c02ca3c.json');
+const serviceAccount =  process.env.NODE_ENV === 'production'  ? require('/etc/secrets/fivlia.json')
+  : require('./fivlia.json');
 
 
 admin.initializeApp({

2ebf0c1b3ffd0e6293b3ebe597dbf83b0e90e692
Goutam <goutam.kathuria@gmail.com>
2025-09-19 18:09:16 +0530

    sfsb

--- config/nodeMailer.js
+++ config/nodeMailer.js
@@ -1,12 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "mail.fivlia.in", // Or use the server IP if given: 172.93.223.239
-  port: 465,              // Use 465 for SSL (recommended)
-  secure: true,           // true for port 465, false for 587
+  host: "smtp.gmail.com",
+ // Or use the server IP if given: 172.93.223.239
+  port: 587,              // Use 465 for SSL (recommended)
+  secure: false,           // true for port 465, false for 587
   auth: {
-    user: "no-reply@fivlia.in",   // Your no-reply email
-    pass: "gx4dBAaGRJ", 
+    user: "fivliaindia@gmail.com",
+    pass: "xybmyypjxwyeldgl", 
   },
   tls: {
     rejectUnauthorized: false, // ⚠️ Only for testing
@@ -17,7 +18,7 @@ const transporter = nodemailer.createTransport({
 const sendVerificationEmail = async (to, subject, html) => {
 
   await transporter.sendMail({
-    from: "Fivlia <no-reply@fivlia.in>",
+    from: "fivliaindia@gmail.com",
     to,
     subject,
     html,

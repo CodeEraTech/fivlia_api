9d35d930c76613728dfdffa4d9c78025c49902b6
Vivek <codeeradevelopers@gmail.com>
2025-10-13 16:54:30 +0530

    chnages

--- firebase/getAccessToken.js
+++ firebase/getAccessToken.js
@@ -3,7 +3,11 @@ const { google } = require("googleapis");
 const path = require("path");
 const fs = require("fs");
 
-const keyPath = path.join(__dirname, "fivlia.json");
+const keyPath =
+  process.env.NODE_ENV === "production"
+    ? require("/etc/secrets/fivlia.json")
+    : path.join(__dirname, "fivlia.json");
+
 const keyFile = JSON.parse(fs.readFileSync(keyPath, "utf8"));
 
 const SCOPES = ["https://www.googleapis.com/auth/firebase.messaging"];
@@ -19,7 +23,6 @@ const getAccessToken = async () => {
     const tokens = await jwtClient.authorize();
     console.log(tokens.access_token);
     return tokens.access_token;
-    
   } catch (err) {
     console.error("❌ Failed to get access token:", err.message);
     return null;

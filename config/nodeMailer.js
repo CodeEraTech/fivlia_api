994842b726eeab73abb2a8c714f9290709836e5d
Goutam <goutam.kathuria@gmail.com>
2025-11-03 14:34:37 +0530

    code of vivek with old email fivlia india

--- config/nodeMailer.js
+++ config/nodeMailer.js
@@ -1,42 +1,34 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "mail.fivlia.com", // Or use the server IP if given: 172.93.223.239
+  host: "smtp.gmail.com", // Or use the server IP if given: 172.93.223.239
   port: 465,              // Use 465 for SSL (recommended)
-  secure: true,         // true for port 465, false for 587
+  secure: true,           // true for port 465, false for 587
   auth: {
-    user: "no-reply@fivlia.com",   // Your no-reply email
-    pass: "2025@Fivlia!", 
+    user: "fivliaindia@gmail.com",   // Your no-reply email
+    pass: "xybmyypjxwyeldgl", 
   },
-  //   tls: {
-  //   rejectUnauthorized: false
-  // }
+  
 });
 
 const sendVerificationEmail = async (to, firstName, lastName, storeName) => {
-  try {
-    await transporter.sendMail({
-      from: "Fivlia <no-reply@fivlia.com>",
-      to,
-      subject: "Fivlia Seller Registration",
-      html: `
+
+  await transporter.sendMail({
+    from: "Fivlia <fivliaindia@gmail.com>",
+    to,
+    subject: "Fivlia Seller Registration",
+    html: `
         <h3>Hello ${firstName} ${lastName},</h3>
         <p>Thank you for registering your store <strong>${storeName}</strong> with Fivlia.</p>
         <p>Your account is under verification. You will be notified once it's approved by our team.</p>
         <p>Regards,<br/>Fivlia Team</p>
       `
-    });
-
-    console.log("Verification email sent to:", to);
-
-  } catch (error) {
-    console.error("Error sending verification email:", error);
-  }
+  });
 };
 
 const sendMailContact = async (to, subject,userEmail, htmlContent) => {
   await transporter.sendMail({
-    from: "Fivlia <no-reply@fivlia.com>",
+    from: "Fivlia <fivliaindia@gmail.com>",
     replyTo: userEmail,
     to,
     subject,

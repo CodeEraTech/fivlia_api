6927aa5fedfdc1840a60166de136a9151a228cf4
Goutam <goutam.kathuria@gmail.com>
2025-11-20 12:41:52 +0530

    csv updates for unbranded

--- controlers/driverControler.js
+++ controlers/driverControler.js
@@ -101,7 +101,7 @@ exports.driverOrderStatus = async (req, res) => {
     const { orderStatus, orderId, otp } = req.body;
 
     // ===> On The Way block
-    if (orderStatus === "On Way") {
+    if (orderStatus === "On The Way") {       //On Way
       const setting = await SettingAdmin.findOne();
       const authSettings = setting?.Auth?.[0] || {};
 
